190746c37d603c428ed790bb7f0e1153bd3d2ebc
quaatos <93578678+quaatos@users.noreply.github.com>
2022-04-14 17:49:02 +0200

    Add files via upload

--- sql/import.sql
+++ sql/import.sql
@@ -9,7 +9,29 @@ CREATE TABLE `data` (
 );
 
 INSERT INTO `data` (name, age, Email)
-VALUES ('quaatos', '17', 'quaatos@gmail.com')
+VALUES ('quaatos', '17', 'quaatos@gmail.com');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('joe', '69', 'joe@mama.hub');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('bardo', '45', 'whois@bardo.com');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('emil', '10', 'emil@protonmail.tor');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('arnold', '78', 'ArnoldSn@whaha.zet');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('upsilon', '1200', 'ups@postarrive.co.uk');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('vlad', '87', 'vlad@impalor.sl');
+
+INSERT INTO `data` (name, age, Email)
+VALUES ('stalin', '120', 'stalin@ussr.ru');
+
 
 --add whatever you want to add.
 --Do not forget, that if you're gonna add stuff in this file, that you will have to update the php
